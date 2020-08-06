499dc2d4d32c00bb1b714825eadb5c96460d0f6f
hidangel7 <64429270+hidangel7@users.noreply.github.com>
2020-08-06 07:41:56 -0400

    updating

--- ppizza/pizzeria/pizzeria/settings.py
+++ ppizza/pizzeria/pizzeria/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pedidos.app.PedidosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
